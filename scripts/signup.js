08e5b363e7dd2201f8c1e3e340a2933267aea2cc
mendsxyz <princemendie03@gmail.com>
2025-08-12 03:05:31 +0100

    update

--- scripts/signup.js
+++ scripts/signup.js
@@ -4,12 +4,32 @@ import { setLoading } from './helpers.js';
 const signupForm = document.querySelector('.signup-form');
 const signupBtn = document.querySelector('.signup-btn');
 
-signupForm.addEventListener('submit', (e) => {
+signupForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   setLoading(signupBtn, true);
 
-  const email = signupForm.querySelector('#set-email').value.trim();
-  const password = signupForm.querySelector('#set-password').value;
-  
-  
+  const email = signupForm.querySelector("#set-email").value.trim();
+  const password = signupForm.querySelector("#set-password").value;
+
+  try {
+    const res = await fetch(
+      "https://script.google.com/macros/s/AKfycbxMFdY_PIWkpjhCk-U35O_hxBlfXNR8oSCpnxxm32s3TgBuPftU4IXhWdkAxweYq1Ee-g/exec",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify([email, password]),
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const data = await res.json();
+    console.log("Response:", data);
+  } catch (err) {
+    console.error("Fetch error:", err);
+  } finally {
+    setLoading(signupBtn, false);
+  }
 });
